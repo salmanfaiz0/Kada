57344bb8a3fc008e78149870d214626a1c298760
salman faiz <18ncs20@meaec.edu.in>
2023-02-13 17:44:08 +0530

    add items in  offer page

--- lib/view/offerpage.dart
+++ lib/view/offerpage.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 class OfferPage extends StatelessWidget {
-  const OfferPage({super.key});
+  List offer = [
+    "Women Jeans up to 60%off",
+    "Men Joggers up to 90%off",
+    "Polo up to 60%off",
+    "Men's Tees up to 10%off",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("OfferPage")),
-    );
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: SafeArea(
+          child: ListView.builder(
+            itemCount: offer.length,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.only(
+                left: 8,
+                right: 8,
+              ),
+              child: Card(
+                child: ListTile(
+                  leading: Container(
+                      width: 33,
+                      height: 33,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.lightBlue[200]),
+                      child: Icon(Icons.discount_rounded)),
+                  title: Text("Offer"),
+                  subtitle: Text(offer[index]),
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
